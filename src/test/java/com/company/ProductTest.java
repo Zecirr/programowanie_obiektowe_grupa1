e2047dfaa8d42dea92fdfd4c02d7f83f0c495967
Your Name <you@example.com>
2023-04-28 11:41:47 +0200

    lab8 z1c,d

--- src/test/java/com/company/ProductTest.java
+++ src/test/java/com/company/ProductTest.java
@@ -1,6 +1,12 @@
 package com.company;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
@@ -25,4 +31,23 @@ class ProductTest {
         });
     }
 
+    private static Stream<Arguments> provideStringsForIsBlank() {
+        return Stream.of(
+                Arguments.of(3, 2010, 2),
+                Arguments.of(12,2012,35),
+                Arguments.of(5,2020,124)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideStringsForIsBlank")
+    void test2(int month, int year, int expected){
+        assertEquals(expected, Product.priceIndex(year,month));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/data.csv", numLinesToSkip = 1, delimiter = ';')
+    void test3(int month, int year, int expected){
+        assertEquals(expected, Product.priceIndex(year,month));
+    }
 }
